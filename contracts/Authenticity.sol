b59795f57dc3197b794ed394a4050946a19abcd5
skarard <skarard@gmail.com>
2023-02-01 20:12:46 +0000

    Resolve malicious actors and clashing hashes

--- contracts/Authenticity.sol
+++ contracts/Authenticity.sol
@@ -2,23 +2,28 @@
 pragma solidity 0.8.14;
 
 contract Authenticity {
-    mapping(bytes32 => address) private _hashAddress;
+    error ArrayLengthMismatch();
+
+    mapping(address => mapping(bytes32 => bool)) private _hashMap;
 
     constructor() {}
 
     function writeHash(bytes32[] memory hash_) external {
         for (uint256 i; i < hash_.length; i++) {
-            _hashAddress[hash_[i]] = msg.sender;
+            _hashMap[msg.sender][hash_[i]] = true;
         }
     }
 
     function readHash(
+        address[] calldata address_,
         bytes32[] calldata hash_
-    ) external view returns (address[] memory) {
-        address[] memory addresses = new address[](hash_.length);
+    ) external view returns (bool[] memory) {
+        if (address_.length != hash_.length) revert ArrayLengthMismatch();
+
+        bool[] memory result = new bool[](hash_.length);
         for (uint256 i; i < hash_.length; i++) {
-            addresses[i] = _hashAddress[hash_[i]];
+            result[i] = _hashMap[address_[i]][hash_[i]];
         }
-        return addresses;
+        return result;
     }
 }
